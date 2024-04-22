81d6fe8035ef6360bc80c7e7f429d61f4c3d5211
FlareEdits <90236206+FlareEdits@users.noreply.github.com>
2024-04-22 16:51:39 -0400

    Add files via upload

---
+++ ArjunRavi_Lab0.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author arjun
+ */
+public class Lab0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hi, my name is Arjun Ravi");
+       System.out.println("My major is Computer Engineering Technology");
+       System.out.println("My favorite progamming language is Java");
+    }
+    
+}
